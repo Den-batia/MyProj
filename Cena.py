ca5d06f7959b726a7400750a43c14772474aac63
Den-batia <denis.batia004@gmail.com>
2020-02-17 20:07:21 +0300

    ok

--- Cena.py
+++ Cena.py
@@ -2,7 +2,8 @@ import threading
 
 from datetime import *
 import requests
-from Main import *
+
+
 sell_list= "https://localbitcoins.net/ru/buy-bitcoins-online/byn/"
 url = sell_list + '.json'
 proxy_logpas = '//user39773:rdwa08@'
@@ -10,15 +11,19 @@ proxy_logpas = '//user39773:rdwa08@'
 
 class Cena(threading.Thread):
 
-    def __init__(self, proxy, time):
+    def __init__(self, proxy, tm):
         super(Cena, self).__init__()
         self.proxy = proxy
-        self.time = time
+        self.tm = tm
 
     def run(self) -> None:
+
         t_1 = datetime.now()
         s = requests.Session()
         s.proxies = {'http': proxy_logpas + self.proxy}
         r = s.get(url).json()['data']['ad_list']
         t_2 = datetime.now()
-        print(t_2 - t_1)
+        self.tm.a((t_2-t_1).microseconds / 1000000, r)
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from threading import Thread
-import time
-from random import randint
-global price
-
 import Cena
 
-acena = Cena.Cena('134.0.116.135:3128', 1)
-acena.start()
+r = {}
+price = 0
 
+
+class Tm:
+    def a(self, time, map):
+        global price, r
+        r = map
+        price = (price + time) / 2
+
+
+tm = Tm()
+a = Cena.Cena('134.0.116.135:3128', tm)
+a.start()
+
+a.join()
+print(a.is_alive())
+print(a)
+print(price)
+print(r)
 
