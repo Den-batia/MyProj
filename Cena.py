2b729e7cb91cad426fb11aa17ee22355fcd3ffb3
Den-batia <denis.batia004@gmail.com>
2020-02-22 14:47:49 +0300

    полное ппц...

--- Cena.py
+++ Cena.py
@@ -2,28 +2,46 @@ import threading
 
 from datetime import *
 import requests
+import json
+import time
+import math
+import random
 
-
-sell_list= "https://localbitcoins.net/ru/buy-bitcoins-online/byn/"
+sell_list = "https://localbitcoins.net/ru/buy-bitcoins-online/byn/"
 url = sell_list + '.json'
 proxy_logpas = '//user39773:rdwa08@'
 
 
 class Cena(threading.Thread):
-
     def __init__(self, proxy, tm):
         super(Cena, self).__init__()
         self.proxy = proxy
         self.tm = tm
 
     def run(self) -> None:
-
         t_1 = datetime.now()
         s = requests.Session()
+        # print(self.proxy + str(self.tm))
         s.proxies = {'http': proxy_logpas + self.proxy}
-        r = s.get(url).json()['data']['ad_list']
+        r = s.get(url).json()
         t_2 = datetime.now()
-        self.tm.a((t_2-t_1).microseconds / 1000000, r)
+        self.tm.a((t_2 - t_1).microseconds / 1000000, r)
+        #print(self.proxy + threading.currentThread().name)
 
+
+class Torg(threading.Thread):
+    def __init__(self, tm):
+        super(Torg, self).__init__()
+        self.tm = tm
+
+    def run(self) -> None:
+
+        while True:
+            r = self.tm.rr()
+            if r is None:
+                continue
+            else:
+                print(r.text)
+            time.sleep(0.3)
 
 
--- main.py
+++ main.py
@@ -1,23 +1,55 @@
 import Cena
+import time
+import random
+import threading
+import concurrent.futures
+import requests
 
-r = {}
-price = 0
+sell_list = "https://localbitcoins.net/ru/buy-bitcoins-online/byn/"
+url = sell_list + '.json'
+time_sleep = 0
+d = {1, 2, 3}
+proxy = ['134.0.116.135:3128', '193.124.46.245:3128', '193.124.46.39:3128',
+         '193.124.46.64:3128', '193.124.46.248:3128', '193.124.46.235:3128',
+         '193.124.46.236:3128', '193.124.46.149:3128', '193.124.46.176:3128',
+         '193.124.46.85:3128']
+
+proxy_logpas = '//user39773:rdwa08@'
 
 
 class Tm:
-    def a(self, time, map):
-        global price, r
-        r = map
-        price = (price + time) / 2
+
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.r = None
+
+    def a(self, time, r):
+        global time_sleep
+        with self.lock:
+            self.r = r
+            print(self.r)
+            # time_sleep = (time_sleep + time) / 2
 
 
-tm = Tm()
-a = Cena.Cena('134.0.116.135:3128', tm)
-a.start()
+    def rr(self):
+        with self.lock:
+            return self.r
 
-a.join()
-print(a.is_alive())
-print(a)
-print(price)
-print(r)
 
+if __name__ == '__main__':
+    tm = Tm()
+    s = requests.Session()
+    Cena.Torg(tm)
+    while True:
+        for pr in range(9):
+            a = Cena.Cena(proxy[pr], tm)
+            a.start()
+
+            time.sleep(0.3)
+        print(threading.active_count())
+
+    # a.join()
+    # print(a.is_alive())
+    # print(a)
+    # print(price)
+    # print(r)r
