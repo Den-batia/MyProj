3307034c56b93700e1d38fbcd824cd856132ef52
Den-batia <denis.batia004@gmail.com>
2020-02-28 22:27:45 +0300

    так уже лучше...

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-import Cena
-import time
-import random
+import threads_methods
+import conf
 import threading
 import re
-import datetime
+from concurrent import futures
 import time
 
 time_sleep = 0
-
-proxy = ['193.111.152.224:4734', '193.111.152.95:4734', '193.111.152.122:4734',
-         '193.111.152.98:4734', '193.111.152.239:4734', '93.111.152.106:4734',
-         '193.111.152.248:4734', '193.111.152.119:4734', '193.111.152.80:4734',
-         '193.111.152.185:4734', '193.111.152.36:4734', '193.111.152.238:4734',
-         '193.111.152.85:4734', '193.111.152.57:4734', '193.111.152.116:4734',
-         '193.111.152.14:4734', '193.111.152.28:4734', '193.111.152.207:4734',
-         '193.111.152.32:4734', '193.111.152.164:4734', '193.111.152.117:4734',
-         '193.111.152.209:4734', '193.111.152.129:4734', '193.111.152.251:4734',
-         '193.111.152.8:4734', '193.111.152.89:4734', '193.111.152.186:4734',
-         '193.111.152.38:4734', '193.111.152.128:4734']
-
 
 class Tm:
 
@@ -36,11 +23,9 @@ class Tm:
             for x in list_price:
                 t = re.search(r'\d{2}\D\d{3}\D\d{2}', x)
                 t = t.span()
-                self.r.append(x[t[0]:t[1]])
+                self.r.append(float(x[t[0]:t[1]].replace(',', '')))
             self.r.sort()
             # t = re.search(r'\d{2}\D\d{3}\D\d{2}', self.r[0])
-            # print(self.r[0][63:72])
-            # print(self.r)
             time_sleep = (time_sleep + time) / 2
 
     def rr(self):
@@ -50,11 +35,10 @@ class Tm:
 
 if __name__ == '__main__':
     tm = Tm()
+    with  futures.ThreadPoolExecutor(max_workers=6) as pool:
+        pool.submit(threads_methods.start1, tm)
+        while True:
+            for pr in conf.proxy:
+                pool.submit(threads_methods.start, pr, tm)
+                time.sleep(time_sleep/3)
 
-    Cena.Torg(tm)
-    while True:
-        for pr in proxy:
-            a = Cena.Cena(pr, tm)
-            a.start()
-            time.sleep(time_sleep)
-            print(time_sleep)
---
+++ threads_methods.py
@@ -0,0 +1,25 @@
+from datetime import *
+import requests
+import time
+import conf
+
+def start(proxy, tm):
+
+    t_1 = datetime.now()
+    s = requests.Session()
+    s.proxies = {'https': conf.proxy_logpas + proxy}
+    r = s.get(conf.url).text
+    s.close()
+    t_2 = datetime.now()
+    time = t_2 - t_1
+    tm.a(time.microseconds / 1000000, r)
+
+def start1(tm):
+
+    while True:
+        r = tm.rr()
+        if r is None:
+            continue
+        else:
+            print(r, end='\n\n')
+        time.sleep(0.2)
